9178815e1e4a4b493dd762f93042834813b03843
Mojtaba Michael <mojimich2015@gmail.com>
2022-08-06 01:23:27 +0430

    It has been added HelloView ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         data = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
@@ -11,3 +15,24 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs'
         ]
         return Response({'status': 'success', 'message': 'Hello', 'data': data})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({"status": "success", "message": message})
+        else:
+            return Response(
+                {"status": "false", "message": "Bad request", "data": {"errors": serializer.errors}},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    def put(self, request, pk=None):
+        return Response({"method": "PUT"})
+
+    def patch(self, request, pk=None):
+        return Response({"method": "PATCH"})
+
+    def delete(self, request, pk=None):
+        return Response({"method": "DELETE"})
