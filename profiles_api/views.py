1e5aee4d4b5ceb868d5bce4ef914305557b3e36e
Mojtaba Michael <mojimich2015@gmail.com>
2022-08-07 20:29:13 +0430

    It has been added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status, viewsets, filters
 from . import serializers
 from profiles_api import models, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -83,3 +85,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
